90c8bb7b06b84de791e54ccc31449d31b33fb655
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-03-31 18:23:12 -0300

    Exceções

--- ong/src/main/java/uol/compass/ong/exceptions/RestExceptionHandler.java
+++ ong/src/main/java/uol/compass/ong/exceptions/RestExceptionHandler.java
@@ -15,9 +15,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-
-
-
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler{
 
