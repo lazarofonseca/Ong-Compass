abb90f19400d7a7629bb87c62ab2e728403ede84
Gabriel Coelho <gabriel.coelhox@gmail.com>
2022-04-05 10:05:54 -0300

    Implementando a entidade resgate

--- ong/src/main/java/uol/compass/ong/entities/Resgate.java
+++ ong/src/main/java/uol/compass/ong/entities/Resgate.java
@@ -1,8 +1,45 @@
 package uol.compass.ong.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import uol.compass.ong.entities.dto.ResgateDTO;
+import uol.compass.ong.enums.Status;
+
+@Entity
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Resgate {
 	
-	private String nome;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id_Resgate;
+	
+	private String endereco;
+	private String caracteristicas_animal;
+	private String descricao;
+	private String usuario;
+	
+	@Enumerated(EnumType.STRING)
+	private Status status;
 
+	public Resgate(ResgateDTO resgateDTO) {
+		this.endereco = resgateDTO.getEndereco();
+		this.caracteristicas_animal = resgateDTO.getCaracteristicas_animal();
+		this.descricao = resgateDTO.getDescricao();
+		this.usuario = resgateDTO.getUsuario();
+		this.status = resgateDTO.getStatus();
+	}
 }
---
+++ ong/src/main/java/uol/compass/ong/repository/ResgateRepository.java
@@ -0,0 +1,8 @@
+package uol.compass.ong.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import uol.compass.ong.entities.Resgate;
+
+public interface ResgateRepository extends JpaRepository<Resgate, Long> {
+}
